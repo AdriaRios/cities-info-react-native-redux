23cf84d4971996e723d106a5374661db762bb7cc
Adria Riosclear <adrianrios89@gmail.com>
2017-02-08 15:41:55 +0100

    Finish test all reducers

--- __tests__/models/adapter/ReduxORMAdapter.js
+++ __tests__/models/adapter/ReduxORMAdapter.js
@@ -1,34 +1,25 @@
 export class ReduxORMAdapter {
     constructor(session) {
-        this.session = session;
-    }
+    this.session = session;
+  }
 
-    build(modelName, props) {
-        return this.session[modelName].create(props);
-    }
+  build(modelName, props) {
+    return this.session[modelName].create(props);
+  }
 
-    save(model, Model) {
-        return Promise.resolve();
-    }
+  async save(model, Model) {
+   return model;
+  }
 
-    // get(doc, attr) {
-    //     console.log('***************get', doc, attr);
-    //     return doc[attr];
-    // }
-    //
-    // set(props, doc) {
-    //     console.log('***************set', props, doc);
-    //     doc.update(props);
-    // }
-    //
-    // save(doc, modelName, cb) {
-    //     console.log('***************save', doc, modelName, cb);
-    //     process.nextTick(cb);
-    // }
-    //
-    // destroy(doc, modelName, cb) {
-    //     console.log('***************destroy', doc, modelName, cb);
-    //     doc.delete();
-    //     process.nextTick(cb);
-    // }
+  async destroy(model, Model) {
+    return Promise.resolve(model.destroy()).then(() => model);
+  }
+
+  get(model, attr) {
+    return model[attr];
+  }
+
+  set(props, model, Model) {
+    return model.set(props);
+  }
 }
--- __tests__/models/models.spec.js
+++ __tests__/models/models.spec.js
@@ -25,6 +25,7 @@ describe('ReduxORM models', () => {
     });
 
     it('Should have two cities', ()=>{
+
         expect(session.City.count()).toEqual(2);
     })
     it('Should have two cities weather', ()=>{
@@ -32,6 +33,33 @@ describe('ReduxORM models', () => {
     })
     it('Should have two cities info', ()=>{
         expect(session.CityInfo.count()).toEqual(2);
+    })
+
+    it('Should create one more City when action dispatched', ()=>{
+        const citiesBeforeAction = session.City.count();
+        const citiesAfterAction = session.City.count() + 1;
+
+        const city = session.City;
+        session.City.reducer({type: 'ADD_CITY_WEATHER', id: citiesBeforeAction + 1, name:'TERRASSA', cityWeatherId: 1} , city, session);
+
+        expect(session.City.count()).toEqual(citiesAfterAction);
+    })
+
+    it('Should create one more City Weather when action dispatched', ()=>{
+        const citiesWeatherAfterAction = session.CityWeather.count() + 1;
+
+        const city = session.CityWeather;
+        session.CityWeather.reducer({type: 'ADD_CITY_WEATHER', weatherIndo: {info: 'My Info'}} , city, session);
+
+        expect(session.CityWeather.count()).toEqual(citiesWeatherAfterAction);
+    })
+
+    it('Should create one more City Weather when action dispatched', ()=>{
+        const citiesInfoAfterAction = session.CityInfo.count() + 1;
+
+        const cityInfo = session.CityInfo;
+        session.CityInfo.reducer({type: 'ADD_CITY_INFO', location: {lat:50, lng: 25}, address:'Desengaño 21'} , cityInfo, session);
+        expect(session.CityInfo.count()).toEqual(citiesInfoAfterAction);
     })
 
 })
